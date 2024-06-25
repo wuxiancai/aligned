c4627416bb3c464d0e713b43a1b4c88786a76228
xiancai <36890537+wuxiancai@users.noreply.github.com>
2024-06-25 15:34:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-curl -L https://raw.githubusercontent.com/yetanotherco/aligned_layer/main/batcher/aligned/install_aligned.sh | bash
+curl -L https://raw.githubusercontent.com/yetanotherco/aligned_layer/main/batcher/aligned/install_aligned.sh
+bash install_aligned.sh
 
 source .bashrc
 
-curl -L https://raw.githubusercontent.com/yetanotherco/aligned_layer/main/batcher/aligned/get_proof_test_files.sh | bash
-
+curl -L https://raw.githubusercontent.com/yetanotherco/aligned_layer/main/batcher/aligned/get_proof_test_files.sh
+bash get_proof_test_files.sh
+source .bashrc
 aligned submit  --proving_system SP1  --proof ~/.aligned/test_files/sp1_fibonacci.proof  --vm_program ~/.aligned/test_files/sp1_fibonacci-elf  --aligned_verification_data_path ~/aligned_verification_data --conn wss://batcher.alignedlayer.com
 
 aligned verify-proof-onchain --aligned-verification-data ~/aligned_verification_data/*.json --rpc https://ethereum-holesky-rpc.publicnode.com --chain holesky
