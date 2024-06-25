18a58a2d5fb76093dbe17bf8b514ec5fdea45fcd
xiancai <36890537+wuxiancai@users.noreply.github.com>
2024-06-25 16:30:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ echo "please input: source .bashrc"
 read bashrc
 echo $bashrc
 curl -L https://raw.githubusercontent.com/yetanotherco/aligned_layer/main/batcher/aligned/get_proof_test_files.sh | bash
+echo "please input : source .bashrc"
+read bashrc
+echo $bashrc
 source .bashrc
 aligned submit  --proving_system SP1  --proof ~/.aligned/test_files/sp1_fibonacci.proof  --vm_program ~/.aligned/test_files/sp1_fibonacci-elf  --aligned_verification_data_path ~/aligned_verification_data --conn wss://batcher.alignedlayer.com
 
